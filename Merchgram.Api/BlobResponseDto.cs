3c43f969b7aae07dc52491d02295d6d5e4922225
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2024-02-06 23:14:06 +0100

    added file upload api

---
+++ Merchgram.Api/BlobResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using Merchgram.Api;
+
+namespace service.files
+{
+    public class BlobResponseDto
+    {
+        public BlobResponseDto()
+        {
+            Blob = new BlobDto();
+        }
+
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public BlobDto Blob { get; set; }
+    }
+}
---
+++ Merchgram.Api/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace service.files.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileService _fileService;
+
+        public FilesController(FileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListProfilePosts()
+        {
+            var result = await _fileService.ListAsync();
+            return Ok(result);
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> ListFollowedProfilesPosts()
+        //{
+        //    return Ok();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(IFormFile file)
+        {
+            var result = await _fileService.UploadAsync(file);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePost()
+        {
+            return Ok();
+        }
+    }
+}
